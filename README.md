93c29a6fde217816f3062223e1c01ad4eb895e02
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-07 18:54:37 -0300

    Hover/seleção no menu navigation para desktop
    Alteração de cores (hue)
    Inclusão das seções contact e footer no ScrollReveal
    Atualização do README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DoctorCare
-### Projeto [DoctorCare](https://www.figma.com/community/file/1102912263666619803), sendo feito na [NLW Return](https://lp.rocketseat.com.br/nlw-return) (Next Level Week #8) da [:rocket:Rocketseat](https://www.rocketseat.com.br).
+### Projeto [DoctorCare](https://www.figma.com/community/file/1102912263666619803), feito na [NLW Return](https://lp.rocketseat.com.br/nlw-return) (Next Level Week #8) da [:rocket:Rocketseat](https://www.rocketseat.com.br).
  
-:construction:  *Deploy: https://luizgustavoabreu.github.io/doctorcare*  :construction:
+Deploy: https://luizgustavoabreu.github.io/doctorcare
 <br><br>
  
 <kbd>[![Preview DoctorCare](https://user-images.githubusercontent.com/72631018/166468425-68833595-f789-416b-a2ff-477b81e06e02.png)](https://luizgustavoabreu.github.io/doctorcare)</kbd>
--- main.js
+++ main.js
@@ -4,6 +4,39 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verifica se a seção passou da linha alvo
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  // verificar se a base está abaixo da linha alvo
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight;
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
@@ -43,4 +76,6 @@ ScrollReveal({
   #services .card,
   #about,
   #about header,
-  #about .content`);
+  #about .content,
+  #contact,
+  footer`);
